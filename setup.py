deab95c129b92ae67e11b44932d1f8e758905d49
Grzegorz Rucinski <grzegorz.rucinski@gemius.com>
2015-11-07 22:25:30 +0100

    Bump version number to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Arango',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/grucin/flask-arango',
     license='Apache License, 2.0',
     author='Grzegorz Rucinski',
